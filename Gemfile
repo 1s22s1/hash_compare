32541756e200cdd9d0511575e4a8870f57891407
1s22s1 <1s22s1@users.noreply.github.com>
2025-04-03 10:56:21 +0900

    First commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in hash_compare.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/hash_compare.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hash_compare/version"
+
+module HashCompare
+  class Error < StandardError; end
+  # Your code goes here...
+end
