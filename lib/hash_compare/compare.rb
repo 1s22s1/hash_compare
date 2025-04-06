155e0fbe7f413c14f603738ce75dd264d7ee9180
1s22s1 <1s22s1@users.noreply.github.com>
2025-04-06 12:13:25 +0900

    nilの場合を考慮しました

--- lib/hash_compare/compare.rb
+++ lib/hash_compare/compare.rb
@@ -5,6 +5,8 @@ module HashCompare
   def compare(comp_a, comp_b)
     return [] if comp_a.nil? && comp_b.nil?
 
+    return [['~', '', comp_a, comp_b]] if comp_a.nil? || comp_b.nil?
+
     lack_key = (comp_a.keys - comp_b.keys).map { |key| ['-', key.to_s, comp_a.fetch(key)] }
     surplus_key = (comp_b.keys - comp_a.keys).map { |key| ['+', key.to_s, comp_b.fetch(key)] }
 
